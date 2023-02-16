97a485b1758ea4adcfd64424528d5e653e519e37
Batan Andrei <yabatan777@gmail.com>
2023-02-16 12:49:53 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,39 +1,48 @@
 const imgURL = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 const img = new Image();
 img.src = imgURL;
 
+const SPEED = 3.1;
+
+let index = 0;
+
 const render = () => {
+  index += 0.3;
+  
+  const backgroudX = -((index * SPEED) % canvas.width);
 
-    const bgSource = {
-      x: 0,
-      y: 0,
-      width: canvas.width,
-      height: canvas.height,
-    }
+  const bgSource = {
+    x: 0,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height,
+  };
 
-    const bgPartOneResult = {
-      x: 0,
-      y: 0,
-      width: canvas.width,
-      height: canvas.height,
-    };
+  const bgPartOneResult = {
+    x: backgroudX + canvas.width,
+    y: 0,
+    width: canvas.width,
+    height: canvas.height,
+  };
 
-    ctx.drawImage(
-        img,
-         bgSource.x,
-         bgSource.y, 
-         bgSource.width, 
-         bgSource.height, 
+  ctx.drawImage(
+    img,
 
-         bgPartOneResult.x,
-         bgPartOneResult.y, 
-         bgPartOneResult.width, 
-         bgPartOneResult.height,
-         );
-        };
+    bgSource.x,
+    bgSource.y,
+    bgSource.width,
+    bgSource.height,
 
+    bgPartOneResult.x,
+    bgPartOneResult.y,
+    bgPartOneResult.width,
+    bgPartOneResult.height
+  );
+  
+  window.requestAnimationFrame(render);
+};
 img.onload = render;
