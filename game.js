7350d1b6ea337e66dc61f6ab9d9e62d08b2f07aa
Batan Andrei <yabatan777@gmail.com>
2023-03-01 15:38:52 +0300

    initial commit

--- game.js
+++ game.js
@@ -87,139 +87,165 @@
 
 
 
-// class Game {
+class Game {
        
-//     constructor(canvas, ctx, bg){
-//         this.canvas = canvas;
-//         this.ctx = ctx;
-//         this.bg = bg;
-        
-//     }
+    constructor(canvas, ctx, bird, bg, fg, pipeUp, pipeBottom, backgroudX){
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.bg = bg;
+        this.bird = bird;
+        this.fg = fg;
+        this.pipeUp = pipeUp;
+        this.pipeBottom = pipeBottom;
+        this.speedBackGround = 3.1;
+        this.index = 0;
+        this.bird = new Image();
+        this.bg = new Image();
+        this.fg = new Image();
+        this.pipeUp = new Image();
+        this.pipeBottom = new Image();
+        this.backgroudX = backgroudX;
+    }
 
-// draw() {
+canvasGame() {
 
-//     this.canvas = document.getElementById("canvas");
-//     this.ctx = this.canvas.getContext("2d");
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
 
-//     this.bg = new Image();
-//     this.bg.src = 'assets/bg.png';
+    this.bird.src = "assets/bird.png";
+    this.bg.src = "assets/bg.png";
+    this.fg.src = "assets/fg.png";
+    this.pipeUp.src = "assets/pipeUp.png";
+    this.pipeBottom.src = "assets/pipeBottom.png";
+};
 
-//     this.ctx.drawImage(this.bg, 0, 0);
-//     //this.bg.onload
-// }
-  
-// start() {
-//     this.bg.onload = this.draw();
-// }
-// }
+drawBack() {
+    this.index += 0.3;
 
-// let game = new Game();
-// game.draw();
-// game.start();
+    this.backgroudX = -((this.index * this.speedBackGround) % this.canvas.width);
 
-// const canvas = document.getElementById("canvas");
-// const ctx = canvas.getContext("2d");
+    this.ctx.drawImage(this.bg, this.backgroudX + this.canvas.width, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(this.bg, this.backgroudX, 0, this.canvas.width, this.canvas.height);
+    
+    window.requestAnimationFrame(this.drawBack.bind(this));
+}
 
-// const bg = new Image();
-// bg.src = 'assets/bg.png';
+drawPipe() {
+    
+}
 
-// function render() {
+loadResources() {
+    window.onload = (event) => {
+        this.drawGame();
+      };
+ };
 
-//     ctx.drawImage(bg, 0, 0);
-// }
+//  updateGame() {
+    
+//  }
+}
 
-// bg.onload = render;
+let game = new Game();
+game.canvasGame();
+game.drawBack();
+game.loadResources();
+//game.updateGame();
+
+
+//window.requestAnimationFrame(game.draw);
+
+
 
 
 //рабочий код
 
 
-let cvs = document.getElementById("canvas");
-let ctx = cvs.getContext("2d");
+// let cvs = document.getElementById("canvas");
+// let ctx = cvs.getContext("2d");
 
-let bird = new Image();
-let bg = new Image();
-let fg = new Image();
-let pipeUp = new Image();
-let pipeBottom = new Image();
+// let bird = new Image();
+// let bg = new Image();
+// let fg = new Image();
+// let pipeUp = new Image();
+// let pipeBottom = new Image();
 
-bird.src = "assets/bird.png";
-bg.src = "assets/bg.png";
-fg.src = "assets/fg.png";
-pipeUp.src = "assets/pipeUp.png";
-pipeBottom.src = "assets/pipeBottom.png";
+// bird.src = "assets/bird.png";
+// bg.src = "assets/bg.png";
+// fg.src = "assets/fg.png";
+// pipeUp.src = "assets/pipeUp.png";
+// pipeBottom.src = "assets/pipeBottom.png";
 
-// Звуковые файлы
-// let fly = new Audio();
-// let score_audio = new Audio();
+// // Звуковые файлы
+// // let fly = new Audio();
+// // let score_audio = new Audio();
 
-// fly.src = "audio/fly.mp3";
-// score_audio.src = "audio/score.mp3";
+// // fly.src = "audio/fly.mp3";
+// // score_audio.src = "audio/score.mp3";
 
-let gap = 90;
+// let gap = 90;
 
-// При нажатии на какую-либо кнопку
-document.addEventListener("click", moveUp);
+// // При нажатии на какую-либо кнопку
+// document.addEventListener("click", moveUp);
 
-function moveUp() {
- yPos -= 25;
- //fly.play();
-}
+// function moveUp() {
+//  yPos -= 25;
+//  //fly.play();
+// }
 
-// Создание блоков
-let pipe = [];
+// // Создание блоков
+// let pipe = [];
 
-pipe[0] = {
- x : cvs.width,
- y : 0
-}
+// pipe[0] = {
+//  x : cvs.width,
+//  y : 0
+// }
 
-let score = 0;
-// Позиция птички
-let xPos = 10;
-let yPos = 150;
-let grav = 1.5;
+// let score = 0;
+// // Позиция птички
+// let xPos = 10;
+// let yPos = 150;
+// let grav = 1.5;
 
-function draw() {
- ctx.drawImage(bg, 0, 0);
+// function draw() {
+//  ctx.drawImage(bg, 0, 0);
 
- for(var i = 0; i < pipe.length; i++) {
- ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
- ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+//  for(var i = 0; i < pipe.length; i++) {
+//  ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+//  ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
- pipe[i].x--;
+//  pipe[i].x--;
 
- if(pipe[i].x == 125) {
- pipe.push({
- x : cvs.width,
- y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
- });
- }
+//  if(pipe[i].x == 125) {
+//  pipe.push({
+//  x : cvs.width,
+//  y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+//  });
+//  }
 
- // Отслеживание прикосновений
- if(xPos + bird.width >= pipe[i].x
- && xPos <= pipe[i].x + pipeUp.width
- && (yPos <= pipe[i].y + pipeUp.height
- || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
- location.reload(); // Перезагрузка страницы
- }
+//  // Отслеживание прикосновений
+//  if(xPos + bird.width >= pipe[i].x
+//  && xPos <= pipe[i].x + pipeUp.width
+//  && (yPos <= pipe[i].y + pipeUp.height
+//  || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+//  location.reload(); // Перезагрузка страницы
+//  }
 
- if(pipe[i].x == 5) {
- score++;
- //score_audio.play();
- }
- }
+//  if(pipe[i].x == 5) {
+//  score++;
+//  //score_audio.play();
+//  }
+//  }
 
- ctx.drawImage(fg, 0, cvs.height - fg.height);
- ctx.drawImage(bird, xPos, yPos);
+//  ctx.drawImage(fg, 0, cvs.height - fg.height);
+//  ctx.drawImage(bird, xPos, yPos);
 
- yPos += grav;
+//  yPos += grav;
 
- ctx.fillStyle = "#000";
- ctx.font = "24px Verdana";
- ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+//  ctx.fillStyle = "#000";
+//  ctx.font = "24px Verdana";
+//  ctx.fillText("Счет: " + score, 10, cvs.height - 20);
 
- requestAnimationFrame(draw);
-}
+//  requestAnimationFrame(draw);
+// }
 
-pipeBottom.onload = draw;
+// pipeBottom.onload = draw;
