a296eb3e74493ae7d0131fb722517ebe665b5060
Batan Andrei <yabatan777@gmail.com>
2023-03-03 22:47:37 +0300

    initial commit

--- src/game.js
+++ src/game.js
@@ -168,8 +168,8 @@ canvasGame() {
 
     this.score = 0;
     this.sizeBird = [34, 26];
-    this.posX = 100;
-    this.posY = 150;
+    this.posX = 50;
+    this.posY = 180;
     this.grav = 1.5; 
 };
 
@@ -183,6 +183,8 @@ drawBack() {
 }
 
 drawBird() {
+        //this.rotation = 30;
+
         this.birdSource = {
                 x: 276,
                 y: 114 + (Math.floor((this.index % 9) / 3) * this.sizeBird[1]),
@@ -196,7 +198,10 @@ drawBird() {
                 width: this.sizeBird[0],
                 height: this.sizeBird[1],
               };
-              
+            //   this.ctx.save();
+            //   this.ctx.translate(this.posX + this.sizeBird[0] / 2, this.posX + this.sizeBird[1] / 2);
+            //   this.ctx.rotate(this.rotation * Math.PI / 180);
+
               this.ctx.drawImage(
                 this.bird,
             
@@ -208,8 +213,9 @@ drawBird() {
                 this.birdResult.x,
                 this.birdResult.y,
                 this.birdResult.width,
-                this.birdResult.height
+                this.birdResult.height,
               );
+              //this.ctx.restore();
 
               this.posY += this.grav;
 
@@ -292,7 +298,7 @@ deadBird() {
         && this.posX <= this.pipe[this.i].x + this.pipeUp.width
         && (this.posY <= this.pipe[this.i].y + this.pipeUp.height
         || this.posY + this.sizeBird[1] >= this.pipe[this.i].y + this.pipeUp.height + this.gap) || this.posY + this.sizeBird[1] >= this.canvas.height - this.fg.height) {
-    
+     
         this.textGameOver();
         this.tabScore();
         this.butStart();
@@ -302,7 +308,6 @@ deadBird() {
 
         this.ctx.font = "bold 22px Verdana";
         this.ctx.fillStyle = "#000";
-
         this.ctx.fillText(this.score, 200, this.canvas.height - 282);
         this.ctx.fillText(this.scoreRec, 200, this.canvas.height - 241);
 
@@ -315,18 +320,9 @@ update() {
     this.drawPipe();
     this.drawGround();
     this.drawBird();
-    this.rotateBird();
     
     this.myReq = window.requestAnimationFrame(this.update.bind(this));
  }
-
- rotateBird() {
-        this.ctx.save();
-        this.ctx.translate(this.x + this.birdD.width / 2, this.y + this.birdD.height / 2);
-        this.ctx.rotate(this.rotation * Math.PI / 180);
-        this.ctx.drawImage(this.birdD, -this.birdD.width / 2, -this.birdD.height / 2);
-        this.ctx.restore();
-    };
  
  textGameOver() {
     this.endGameSource = {
@@ -446,100 +442,3 @@ game.update();
 
 
 
-
-
-
-
-
-//рабочий код
-
-
-// let cvs = document.getElementById("canvas");
-// let ctx = cvs.getContext("2d");
-
-// let bird = new Image();
-// let bg = new Image();
-// let fg = new Image();
-// let pipeUp = new Image();
-// let pipeBottom = new Image();
-
-// bird.src = "assets/bird.png";
-// bg.src = "assets/bg.png";
-// fg.src = "assets/fg.png";
-// pipeUp.src = "assets/pipeUp.png";
-// pipeBottom.src = "assets/pipeBottom.png";
-
-// // Звуковые файлы
-// // let fly = new Audio();
-// // let score_audio = new Audio();
-
-// // fly.src = "audio/fly.mp3";
-// // score_audio.src = "audio/score.mp3";
-
-// let gap = 90;
-
-// // При нажатии на какую-либо кнопку
-// document.addEventListener("click", moveUp);
-
-// function moveUp() {
-//  yPos -= 25;
-//  //fly.play();
-// }
-
-// // Создание блоков
-// let pipe = [];
-
-// pipe[0] = {
-//  x : cvs.width,
-//  y : 0
-// }
-
-// let score = 0;
-// // Позиция птички
-// let xPos = 10;
-// let yPos = 150;
-// let grav = 1.5;
-
-// function draw() {
-//  ctx.drawImage(bg, 0, 0);
-
-//  for(var i = 0; i < pipe.length; i++) {
-//  ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-//  ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
-
-//  pipe[i].x--;
-
-//  if(pipe[i].x == 125) {
-//  pipe.push({
-//  x : cvs.width,
-//  y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-//  });
-//  }
-
-//  // Отслеживание прикосновений
-//  if(xPos + bird.width >= pipe[i].x
-//  && xPos <= pipe[i].x + pipeUp.width
-//  && (yPos <= pipe[i].y + pipeUp.height
-//  || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
-//  location.reload(); // Перезагрузка страницы
-//  }
-
-//  if(pipe[i].x == 5) {
-//  score++;
-//  //score_audio.play();
-//  }
-//  }
-
-//  ctx.drawImage(fg, 0, cvs.height - fg.height);
-//  ctx.drawImage(bird, xPos, yPos);
-
-//  yPos += grav;
-
-//  ctx.fillStyle = "#000";
-//  ctx.font = "24px Verdana";
-//  ctx.fillText("Счет: " + score, 10, cvs.height - 20);
-
-//  requestAnimationFrame(draw);
-// }
-
-// pipeBottom.onload = draw;
